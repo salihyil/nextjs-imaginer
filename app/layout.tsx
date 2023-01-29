70f62af43d4620ced5119e9764cc2bf429917be5
Salih Yılmaz <salih_yil@live.com>
2023-01-30 00:06:06 +0300

    create app, global css and reset css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
-import './globals.css'
+import '@/styles/reset.css'
+import '@/styles/variables.css'
+import '@/styles/global.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <body className='layout'>{children}
+        <div className='overlay' /></body>
     </html>
   )
 }
