1d043e6c37a658b3db362c61ccdd7302d98cd0f9
Salih Yılmaz <salih_yil@live.com>
2023-01-30 00:17:35 +0300

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,15 @@ import '@/styles/reset.css'
 import '@/styles/variables.css'
 import '@/styles/global.css'
 
+import { mainFont } from '@/libs/font'
+
+
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className='layout'>{children}
+    <html lang="en" className={mainFont.className}>
+      <body className='layout '>{children}
         <div className='overlay' /></body>
     </html>
   )
