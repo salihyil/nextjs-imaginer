7f9b0c3865778981762e1e4e476832941a256e0b
Salih Yılmaz <salih_yil@live.com>
2023-01-30 12:14:56 +0300

    converted to single quotation marks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
-import '@/styles/reset.css'
-import '@/styles/variables.css'
-import '@/styles/global.css'
+import '@/styles/reset.css';
+import '@/styles/variables.css';
+import '@/styles/global.css';
 
-import { mainFont } from '@/libs/font'
-
-
-
+import { mainFont } from '@/libs/font';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={mainFont.className}>
-      <body className='layout '>{children}
-        <div className='overlay' /></body>
+    <html lang='en' className={mainFont.className}>
+      <body className='layout '>
+        {children}
+        <div className='overlay' />
+      </body>
     </html>
-  )
+  );
 }
